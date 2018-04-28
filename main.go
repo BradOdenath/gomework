e1cc0f3f574e3dafabf1f0c41f7f77a8c13aa53d
brad <brad.odenath@gmail.com>
2018-04-27 23:57:42 -0400

    AYYYYY

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"bufio"
 	"unicode"
 	"strings"
+	"fmt"
+	"strconv"
 )
 
 const (
@@ -22,8 +24,19 @@ func check(e error){
 }
 
 func main() {
-	log.Println("Gomework!")
-	readCityDocument("dj38.tsp.txt")
+	//Pride
+	fmt.Println("Gomework!")
+
+	//Parse the document for coordiantes
+	cityCoords := readCityDocument("dj38.tsp.txt")
+
+	//Put them things into a thing.
+	cityTour := populateTour(cityCoords)
+
+	//Feels good mate!
+	log.Println(getDistance(cityTour))
+
+
 }
 
 /* ===== CITY ===== */
@@ -46,7 +59,7 @@ func distance(c1 city, c2 city) int64 {
 				Go's structure does not allow pre statement operators.	*/
 	return int64(
 		math.Round(
-			math.Sqrt(math.Pow(c1.x-c2.y, 2) +
+			math.Sqrt(math.Pow(c1.x-c2.x, 2) +
 				math.Pow(c1.y-c2.y, 2))))
 }
 
@@ -56,38 +69,71 @@ func toString(c city) string {
 }
 
 /* ===== TOUR ===== */
-/*
 
+//Note: the city needed can be obtained through accessing the path
+//		directly rather than making a method.
 type tour struct {
-	tourAL[] Channel	//getTour()
-	distance in64	//getistance
+	path[] city
 }
-func createTour() tour {
-	return tour{context.TODO(),0}
+
+func addCity(t *tour, c city) {
+	t.path = append(t.path, c)
+	log.Println("Succesfully added city to tour.")
 }
-*/
 
-/* ===== FILE READER FOR COORDINATES AND CITYLIST ===== */
+func numberOfCities(t tour) int64 {
+	return int64(len(t.path))
+}
 
-type cityList struct {
+func populateTour(coords cityCoordinates) tour {
+	t := tour{}
+	for i, cityCoordX := range coords.cityX { //X same length as Y
+		tempX, err := strconv.ParseFloat(cityCoordX,64)
+		check(err)
+		tempY, err := strconv.ParseFloat(coords.cityY[i],64)
+		check(err)
+		t.path = append(t.path, city{tempX, tempY, default_name})
+	}
+	return t
+}
+
+func getDistance(t tour) int64 {
+	totalDistance := int64(0)
+	for i, c := range t.path {
+		destination := city{}
+
+		if (int64(i + 1) < numberOfCities(t)) {
+			destination = t.path[i+1]
+		} else {
+			destination = t.path[0]
+		}
+
+		totalDistance += distance(c, destination)
+	}
+
+}
+
+/* ===== TOUR MANAGER ===== */
+
+
+
+/* ===== COORDINATE FILE GRABBER STUFF ===== */
+
+type cityCoordinates struct {
 	cityX[] string
 	cityY[] string
 }
 
-func readCityDocument(fileName string) cityList {
-	cities := cityList{}
-
+//Heart palpitations ensued as I wrote this.
+func readCityDocument(fileName string) cityCoordinates {
 	file, err := os.Open(fileName)
 	check(err)
 	defer file.Close()
 
-	//outStr := ""
-
+	cities := cityCoordinates{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		scanStr := scanner.Text()
-		//log.Println(scanStr)
-
 		tempRune := []rune(scanStr)
 
 		if unicode.IsDigit(tempRune[0]) {
@@ -97,12 +143,6 @@ func readCityDocument(fileName string) cityList {
 			cities.cityY = append(cities.cityY, tempCoords[2])
 		}
 	}
-
-	// Testing Purposes
-	/*
-	log.Println("\n", cities.cityX)
-	log.Println("\n", cities.cityY)
-	*/
 
 	return cities
 }
